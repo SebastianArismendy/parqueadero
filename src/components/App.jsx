d6d1af97cd53c8e168b4070ac033e20906ce9601
Sebastian Arismendy <sebastian.arismendy@pragma.com.co>
2023-11-27 22:28:32 -0500

    quitar outlet innecesario

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes, Outlet  } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes  } from "react-router-dom";
 import { ParqueaderoProvider } from "../context/ParqueaderoContext";
 import Login from "./Login.jsx";
 import RegistroVehiculos from "./RegistroVehiculos";
@@ -16,7 +16,6 @@ function App() {
         {shouldShowBottomNav && <BottomNav />}
         <Routes>
           <Route path="/" element={<Login />} />
-          <Route path="/*" element={<MainRoutes />} />
           <Route path="/registro-vehiculos" element={<RegistroVehiculos />} />
           <Route path="/ingreso-vehiculo" element={<IngresoVehiculo />} />
           <Route path="/parqueadero" element={<Parqueadero />} />
@@ -27,13 +26,5 @@ function App() {
   );
 }
 
-const MainRoutes = () => {
-  return (
-    <>
-      <Outlet />
-      <BottomNav />
-    </>
-  );
-};
 
 export default App;
